b6a339b231daa0e61ac025a75fd881ce470bfe55
Adrian Dragota <47034477+ActionAdi83@users.noreply.github.com>
2024-07-17 15:47:39 +0300

    Update rotation-reader.js

--- rotation-reader.js
+++ rotation-reader.js
@@ -3,6 +3,9 @@ AFRAME.registerComponent('rotation-reader', {
     const cameraEl = this.el;
     const rotation = cameraEl.getAttribute('rotation');
     const hud = document.getElementById('hud');
+    if (rotation.x > 90 || rotation.x < 90) {
+      alert('unghiul mai mare de 90 de grade');
+    }
     hud.innerHTML = `Pitch (x): ${rotation.x.toFixed(2)}<br>Yaw (y): ${rotation.y.toFixed(2)}`;
   }
 });
